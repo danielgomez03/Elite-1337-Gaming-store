2c640eedf04389342e55ffb902b038f50406d6e8
Hugo-Val <hvb1507@hotmail.com>
2023-06-23 13:29:45 -0600

    rutas

---
+++ server/src/functions/postCreateProduct.js
@@ -0,0 +1,8 @@
+const { Product, Image, Category } = require('../database');
+
+async function postCreateProduct(req, res) {
+    res.send('postCreateProduct');
+
+}
+
+module.exports = postCreateProduct;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 // Import all routers;
 // Example: const authRouter = require('./auth.js');
+const getAllProducts = require('./GetAllProducts');
+const postCreateProduct = require('./PostCreateProduct');
 
 
 const router = Router();
 
 // Configure routers
 // Example: router.use('/auth', authRouter);
+router.use(getAllProducts);
+router.use(postCreateProduct);
 
 
 module.exports = router;
