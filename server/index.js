92415704c76ab6997a851c6b47a889990e8ba85a
danielgomez03 <Danielgome.serme@gmail.com>
2023-06-23 15:16:00 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/database.js');
 
+const {
+  PORT
+} = process.env;
+
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log('%s listening at ${PORT}'); // eslint-disable-line no-console
   });
 });
