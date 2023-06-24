14af2182854b6ed2c02d98267aa92140b1b217ef
fuegobscuro <nachieee@gmail.com>
2023-06-24 12:03:34 -0300

    updated Token model

--- server/src/models/Token.js
+++ server/src/models/Token.js
@@ -2,15 +2,15 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   
-    sequelize.define('token', {
-  
+  const Token = sequelize.define('token', {
+    
     tokenId: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       allowNull: false,
       primaryKey: true,
     },
-    
+
     userId: {
       type: DataTypes.UUID,
       allowNull: false,
@@ -19,32 +19,43 @@ module.exports = (sequelize) => {
         key: 'userId',
       },
     },
-    
+
+    loginId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'login',
+        key: 'loginId',
+      },
+    },
+
     tokenValue: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    
+
     tokenType: {
-      type: DataTypes.ENUM('auth0', 'google'),
+      type: DataTypes.ENUM('Auth0', 'Google'),
       allowNull: false,
     },
 
     createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
     },
 
     expiresAt: {
-        type: DataTypes.DATE,
-        allowNull: true,
+      type: DataTypes.DATE,
+      allowNull: true,
     },
-    
+
     ipAddress: {
-    type: DataTypes.STRING,
-    allowNull: true,
+      type: DataTypes.STRING,
+      allowNull: true,
     },
 
   }, { timestamps: false });
+
+  return Token;
 };
