85051c96a69a4376daf1053813bfd94c1e3a9d7a
fuegobscuro <nachieee@gmail.com>
2023-06-25 10:47:25 -0300

    added data for users / modified products data

--- server/src/data/users.js
+++ server/src/data/users.js
@@ -1,49 +1,68 @@
 const users = [
   {
-    'userId': 'ef05ef0a-0a35-448d-b8dd-1a9f24f2d557',
-    'firstName': 'John',
-    'lastName': 'Doe',
-    'country': 'United States',
-    'region': 'California',
-    'city': 'Los Angeles',
-    'address': '123 Main St',
-    'postalCode': '12345',
-    'birthDate': '1990-01-01',
-    'phoneNumber': '1234567890',
-    'idNumber': '123456789',
-    'userRole': 'common',
-    'isActive': true,
-    'email': 'johndoe@example.com',
-    'password': 'password123',
-    'image': [
+    firstName: 'John',
+    lastName: 'Doe',
+    country: 'United States',
+    region: 'California',
+    city: 'Los Angeles',
+    address: '123 Main St',
+    postalCode: '12345',
+    birthDate: '1990-01-01',
+    phoneNumber: '1234567890',
+    idNumber: '123456789',
+    userRole: 'common',
+    isActive: true,
+    email: 'johndoe@example.com',
+    password: 'password123',
+    image: [
       {
-        'imageId': 'af05ef0a-0a35-448d-b8dd-1a9f24f2d557',
-        'url': 'https://example.com/image1.jpg',
-        'caption': 'Profile Picture'
+        imageId: 'af05ef0a-0a35-448d-b8dd-1a9f24f2d557',
+        url: 'https://example.com/image1.jpg'
       },
     ]
   },
   {
-    'userId': '3e7c6f07-957e-4e3e-b08d-56d1a6d84179',
-    'firstName': 'Jane',
-    'lastName': 'Smith',
-    'country': 'Canada',
-    'region': 'Ontario',
-    'city': 'Toronto',
-    'address': '456 Elm St',
-    'postalCode': '54321',
-    'birthDate': '1985-05-10',
-    'phoneNumber': '9876543210',
-    'idNumber': '987654321',
-    'userRole': 'common',
-    'isActive': true,
-    'email': 'janesmith@example.com',
-    'password': 'password456',
-    'image': [
+    firstName: 'Jane',
+    lastName: 'Smith',
+    country: 'Canada',
+    region: 'Ontario',
+    city: 'Toronto',
+    address: '456 Elm St',
+    postalCode: '54321',
+    birthDate: '1985-05-10',
+    phoneNumber: '9876543210',
+    idNumber: '987654321',
+    userRole: 'common',
+    isActive: true,
+    email: 'janesmith@example.com',
+    password: 'password456',
+    image: [
       {
-        'imageId': 'bd8f145a-8e6c-4de3-8b8b-9b431f1f29d4',
-        'url': 'https://example.com/image2.jpg',
-        'caption': 'Profile Picture'
+        imageId: 'bd8f145a-8e6c-4de3-8b8b-9b431f1f29d4',
+        url: 'https://example.com/image2.jpg',
+      },
+    ]
+  },
+  {
+    firstName: 'Alex',
+    lastName: 'Johnson',
+    country: 'Australia',
+    region: 'New South Wales',
+    city: 'Sydney',
+    address: '789 Oak St',
+    postalCode: '98765',
+    birthDate: '1995-08-20',
+    phoneNumber: '1239874560',
+    idNumber: '654321987',
+    userRole: 'common',
+    isActive: true,
+    email: 'alexjohnson@example.com',
+    password: 'password789',
+    image: [
+      {
+        imageId: 'cde7f8a1-2b3c-4d5e-6f7g-8h9i0j1k2l3m',
+        url: 'https://example.com/image3.jpg',
+        caption: 'Profile Picture'
       },
     ]
   }
