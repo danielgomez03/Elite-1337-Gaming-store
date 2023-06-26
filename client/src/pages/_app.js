1a6d32a1c3c7603fa85763098e7041b7dc64a7ad
Man.D.Klaus <mansilland@gmail.com>
2023-06-26 14:45:27 -0300

    NavBarGuest

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -3,38 +3,36 @@ import { Provider } from 'react-redux';
 import store from '../redux/store';
 import NavBarUsers from '../components/NavBarUsers';
 import NavBarAdmin from '@/components/NavBarAdmin';
+import NavBarGuest from '@/components/NavBarGuest';
 import Footer from '../components/Footer';
 import Header from '@/components/Header';
 import 'tailwindcss/tailwind.css';
-require('typeface-montserrat')
-require('typeface-roboto')
+require('typeface-montserrat');
+require('typeface-roboto');
 
 function App({ Component, pageProps }) {
-  const [admin, setAdmin] = useState(true); // luego habría que linkearlo con el dato del perfil que llegue desde el back
-
-  function toggleAdmin() {
-    setAdmin((prevAdmin) => !prevAdmin);
-  }  
+  const [typeUser, setTypeUser] = useState('guest');
+  
+  const handleUserChange = (event) => {
+    setTypeUser(event.target.value);
+  };
 
   return (
     <Provider store={store}>
-      <div className="w-full font-montserrat mt-32" >
+      <div className="w-full font-montserrat mt-32">
         <Header />
-        <div className='page-container min-h-[calc(100vh-378px)] flex flex-col justify-center items-center p-4 ' >
-          {admin ? (
-            <div>
-              <NavBarAdmin />
-              <button onClick={toggleAdmin}>admin</button>
-            </div>
-          ) : (
-              <NavBarUsers />
-          )}
-          <Component {...pageProps} />     
+        <select value={typeUser} onChange={handleUserChange}>
+          <option value="admin">Admin</option>
+          <option value="users">Users</option>
+          <option value="guest">Guest</option>
+        </select>
+        <div className='page-container min-h-[calc(100vh-378px)] flex flex-col justify-center items-center p-4'>
+          {typeUser === 'admin' && <NavBarAdmin />}
+          {typeUser === 'users' && <NavBarUsers />}
+          {typeUser === 'guest' && <NavBarGuest />}
+          <Component {...pageProps} />
         </div>
-        <Footer  
-          className='mt-auto' 
-          admin={admin}
-        />
+        <Footer className='mt-auto' />
       </div>
     </Provider>
   );
