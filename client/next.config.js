3b739752b6e52631e6fde285883ad2292b51d2ef
NCenVictoria <122835761+NCenVictoria@users.noreply.github.com>
2023-06-26 13:50:17 -0600

    ...

--- client/next.config.js
+++ client/next.config.js
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-        return [
-          {
-            // Permitir el acceso desde cualquier origen durante el desarrollo
-            source: "/product/create",
-            headers: [
-              {
-                key: "Access-Control-Allow-Origin",
-                value: "*",
-              },
-            ],
-          },
-        ];
-      },
-}
+const nextConfig = {}
+
 
 module.exports = nextConfig
