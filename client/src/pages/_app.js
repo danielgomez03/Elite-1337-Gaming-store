e05b2ba6989d9d36295193d71b75b60046abeb40
NCenVictoria <122835761+NCenVictoria@users.noreply.github.com>
2023-06-26 13:40:03 -0600

    .....

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
+import NavBarUsers from '../components/NavBarUsers';
+import NavBarAdmin from '@/components/NavBarAdmin';
 
+import Footer from '../components/Footer';
+import Header from '@/components/Header';
 import 'tailwindcss/tailwind.css';
 
+
+
 function App({ Component, pageProps }) {
+  const [typeUser, setTypeUser] = useState('guest');
   
+  const handleUserChange = (event) => {
+    setTypeUser(event.target.value);
+  };
 
   return (
     <Provider store={store}>
-   <Component {...pageProps} />
+      <div className="w-full font-montserrat mt-32">
+        <Header />
+        <select value={typeUser} onChange={handleUserChange}>
+          <option value="admin">Admin</option>
+          <option value="users">Users</option>
+          <option value="guest">Guest</option>
+        </select>
+        <div className='page-container min-h-[calc(100vh-378px)] flex flex-col justify-center items-center p-4'>
+          {typeUser === 'admin' && <NavBarAdmin />}
+          {typeUser === 'users' && <NavBarUsers />}
+          {typeUser === 'guest' && <NavBarGuest />}
+          <Component {...pageProps} />
+        </div>
+        <Footer className='mt-auto' />
+      </div>
     </Provider>
   );
 }
