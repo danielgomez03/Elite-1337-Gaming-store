2e18f2543b06988b5065620b0d512b7d07cecc0f
danielgomez03 <Danielgome.serme@gmail.com>
2023-06-28 14:12:43 -0500

    card component pdate

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -18,13 +18,13 @@ import React from 'react'
   // </div>
 
   
-<div key={props.productId} class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<div key={props.productId} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-100 dark:border-gray-700">
     <a href="#">
         <img class="p-8 rounded-t-lg" src="https://http2.mlstatic.com/D_NQ_NP_730943-MCO69677536328_052023-O.webp" alt="product image" />
     </a>
     <div key={props.productId} class="px-5 pb-5">
         <a href="#">
-            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{props.name}</h5>
+            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-black">{props.name}</h5>
         </a>
         <div class="flex items-center mt-2.5 mb-5">
             <svg aria-hidden="true" class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
