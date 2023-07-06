587ba1a2f07a31f119685692adba0f8ccca31087
Man.D.Klaus <mansilland@gmail.com>
2023-07-06 00:53:34 -0300

    Correcciones de className y href

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -21,32 +21,27 @@ export default function SearchBar() {
             dispatch(getProductByName(name))
             dispatch(actionByName())
             setName('');
-
-
         } else
             return alert('enter name')
     }
 
-
-    return (
-        <div className='flex items-center justify-center relative'>
-            <input
-                class='inputs'
-                type='text'
-                value={name}
-                placeholder='Search Product...'
-                onChange={handleInputChange}
-                className="w-full px-4 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-            />
-            <button
-                class='button'
-                type='submit'
-                onClick={handleSubmit}
-                className='absolute right-2'>
-                <span class="material-symbols-rounded pt-1">
-                    search
-                </span>
-            </button>
-        </div>
-    )
+return (
+    <div className='flex items-center justify-center relative'>
+        <input
+            type='text'
+            value={name}
+            placeholder='Search Product...'
+            onChange={handleInputChange}
+            className="w-full px-4 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+        />
+        <button
+            type='submit'
+            onClick={handleSubmit}
+            className='absolute right-2'>
+            <span className="material-symbols-rounded pt-1">
+                search
+            </span>
+        </button>
+    </div>
+)
 }
