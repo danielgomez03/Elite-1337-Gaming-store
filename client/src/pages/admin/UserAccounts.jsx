9f3702d81b0c9014e464c9b4cff00693a828b4b7
NCenVictoria <122835761+NCenVictoria@users.noreply.github.com>
2023-07-07 18:49:20 -0600

    funcion isActive

--- client/src/pages/admin/UserAccounts.jsx
+++ client/src/pages/admin/UserAccounts.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
-
+import { fetchUsers, modifyIsActiveUser } from '@/redux/actions';
+import React, { useEffect , useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 function UserAccounts() {
+  const dispatch = useDispatch();
+  const users = useSelector(state=>state.users)
+  useEffect(()=>{
+ dispatch(fetchUsers())
+
+  },[])
+  const [buttonStates, setButtonStates] = useState({});
+
+  const handleClick = (userId,isActive) => {
+    
+   
+      dispatch(modifyIsActiveUser(userId,isActive)).then(()=>{
+        dispatch(fetchUsers())
+      })
+   
+    
+  };
   return (
-    <div>UserAccounts</div>
+    <div>UserAccounts
+        <div>
+      {users.map((user) => (
+        <div key={user.userId} className="">
+          <span><img src={user.image.url} alt="User Avatar" className="user-avatar" /></span>
+          <span className="user-name">{`${user.firstName} ${user.lastName}`}</span>
+          <button name={user.userId} onClick={() => handleClick(user.userId,user.isActive)}>
+              
+              {user.isActive? 'Activo' : 'Inactivo'}
+            </button>
+ 
+        </div>
+      ))}
+    </div>
+    </div>
   )
 }
 
