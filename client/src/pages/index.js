118ff24c3618272827b8d2d569b32608e8481d17
danielgomez03 <Danielgome.serme@gmail.com>
2023-07-08 09:19:10 -0500

    little fixes

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,16 @@
-import React from 'react'
-import Products from './guest/Products'
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getProducts } from '../redux/actions';
+import Products from './guest/Products';
 
 function Home() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, []);
+
   return (
     <div>
       <Products/>
