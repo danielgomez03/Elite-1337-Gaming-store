a089799e84da6b34618bc57be1ec2f213b2522b8
Man.D.Klaus <mansilland@gmail.com>
2023-07-07 23:35:41 -0300

    Redux para Persistencia de sesión

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -7,7 +7,7 @@ export const GET_CATEGORIES = "GET_CATEGORIES";
 export const GET_CART_BY_ID_USER = "GET_CART_BY_ID_USER";
 export const PAGE = "PAGE";
 export const CLEAN = "CLEAN";
-export const TOTAL_PRODUCTS= "TOTAL_PRODUCTS";
+export const TOTAL_PRODUCTS = "TOTAL_PRODUCTS";
 export const MODIFY_QUANTITY = "MODIFY_QUANTITY";
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const ACTION_BYNAME = "ACTION_BYNAME";
@@ -21,8 +21,8 @@ export const SORT_PRODUCTS = "SORT_PRODUCTS";
 export const FILTER_PRODUCTS_BY_PRICE = "FILTER_PRODUCTS_BY_PRICE";
 export const FILTER_PRODUCTS_BY_CATEGORY = "FILTER_PRODUCTS_BY_CATEGORY";
 //---------Rating types----/
-export const GET_RATINGS ="GET_RATINGS";
-export const GET_RATINGS_ERROR ="GET_RATINGS_ERROR";
+export const GET_RATINGS = "GET_RATINGS";
+export const GET_RATINGS_ERROR = "GET_RATINGS_ERROR";
 export const ADD_RATING = 'ADD_RATING';
 //---------Comments types----/
 export const GET_COMMENTS_BY_PRODUCT = "GET_COMMENTS_BY_PRODUCT";
@@ -33,24 +33,95 @@ export const DELETE_FAVORITE = 'DELETE_FAVORITE';
 export const DELETE_FAVORITE_ERROR = 'DELETE_FAVORITE_ERROR';
 export const GET_FAVORITES_SUCCESS = 'GET_FAVORITES_SUCCESS';
 export const GET_FAVORITES_FAILURE = 'GET_FAVORITES_FAILURE';
+//---------Session types----/
+export const POST_LOGIN = "POST_LOGIN";
+export const POST_LOGOUT = "POST_LOGOUT";
+export const CONFIRM_SESSION = "CONFIRM_SESSION";
 //---------other types----/
 
 
 
 
+
+// -----------actions session----------------------------------------------------------------------------------------
+export const confirmSession = (tokenRedux) => {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get("http://localhost:3001/login/tokens", {
+        headers: {
+          Authorization: `Bearer ${tokenRedux}`,
+        },
+      });
+
+      const { data } = response;
+      if (data && data.length > 0 && data[0] === tokenRedux) {
+        console.log("token", data[0]);
+        dispatch({ type: CONFIRM_SESSION, payload: data[0] });
+      } else {
+        console.log("Not session initialized");
+      };
+    } catch (error) {
+      console.error("Error during login:", error);
+      // Aquí puedes lanzar una acción adicional para manejar el estado de error si es necesario
+    };
+  };
+};
+
+export const postLogin = (tokenRedux, credentials) => {
+  return async function (dispatch) {
+    try {
+      if (tokenRedux === "") {
+        const response = await axios.post("http://localhost:3001/login/signin", credentials);
+        const token = response.data;
+        if (token) {
+          console.log("token", token);
+          dispatch({ type: POST_LOGIN, payload: token });
+        } else {
+          console.log("Login failed");
+        }
+      } else {
+        console.log("Session already initialized");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+};
+
+export const postLogout = (tokenRedux) => {
+  return async function (dispatch) {
+    try {
+      const response = await axios.post("http://localhost:3001/login/signout", null, {
+        headers: {
+          Authorization: `Bearer ${tokenRedux}`,
+        },
+      });
+
+      if (response.data === "Logout Succesful") {
+        const token = "";
+        dispatch({ type: POST_LOGOUT, payload: token });
+      } else {
+        console.log("Logout failed");
+      }
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
+  };
+};
+
 // -----------actions cart----------------------------------------------------------------------------------------
-export const deleteProduct = (product) =>{
+export const deleteProduct = (product) => {
   console.log(product)
   return async function (dispatch) {
-    const response = await axios.delete("http://localhost:3001/carts/remove", {data:product});
+    const response = await axios.delete("http://localhost:3001/carts/remove", { data: product });
     const cart = response.data;
     console.log(cart)
     dispatch({ type: DELETE_PRODUCT, payload: cart });
   };
 }
 
-export const modifyQuantity = (product) =>{
- console.log(product)
+export const modifyQuantity = (product) => {
+  console.log(product)
   return async function (dispatch) {
     const response = await axios.patch("http://localhost:3001/carts/edit", product);
     const cart = response.data;
@@ -58,36 +129,36 @@ export const modifyQuantity = (product) =>{
     dispatch({ type: MODIFY_QUANTITY, payload: cart });
   };
 }
-export const getCartByIdUser = (id) =>{
+export const getCartByIdUser = (id) => {
   return async function (dispatch) {
     const response = await axios.get(`http://localhost:3001/carts/user/${id}`);
     const cart = response.data;
     dispatch({ type: GET_CART_BY_ID_USER, payload: cart });
   };
 }
-export const countCart =()=>{
-  
-  return function  (dispatch){
-  return dispatch({type: TOTAL_PRODUCTS });   
+export const countCart = () => {
+
+  return function (dispatch) {
+    return dispatch({ type: TOTAL_PRODUCTS });
   }
 }
 export const addProductToCart = (id) => {
-  const product={
+  const product = {
     userId: "ac5b18b6-6383-4a9f-8e4c-65ad3c93b81a",
     productId: id,
     quantity: 1
   }
-  
+
   return async function (dispatch) {
-   
-    const response = await axios.post("http://localhost:3001/carts/add",product);
-    
+
+    const response = await axios.post("http://localhost:3001/carts/add", product);
+
     const cart = response.data
     console.log(cart
-      )
-      dispatch({type: ADD_PRODUCT_TO_CART ,payload:cart})
-    };
-  }
+    )
+    dispatch({ type: ADD_PRODUCT_TO_CART, payload: cart })
+  };
+}
 
 //---------------------------------------------------------------------//
 //User's actions---------------------------------//
@@ -119,7 +190,7 @@ export const fetchUserById = (userId) => {
 //---------------------------------------------------------------------//
 //Get Products actions---------------------------------//
 
-  export const getProducts = () => {
+export const getProducts = () => {
   return async function (dispatch) {
     const response = await axios.get("http://localhost:3001/products");
     const products = response.data;
@@ -131,78 +202,78 @@ export const getProductByName = (name) => {
   return async function (dispatch) {
     const bd = await axios.get(`http://localhost:3001/products?name=${name}`);
     const product = bd.data
-    dispatch({ type: GET_PRODUCT_BY_NAME, payload:product }); 
+    dispatch({ type: GET_PRODUCT_BY_NAME, payload: product });
   };
 };
 
 export const getProductById = (id) => {
-    return async function (dispatch) {
-      const bd = await axios.get(`http://localhost:3001/products/id/${id}`);
-      const detail = bd.data
-      dispatch({ type: GET_PRODUCT_BY_ID, payload:detail }); 
-    };
+  return async function (dispatch) {
+    const bd = await axios.get(`http://localhost:3001/products/id/${id}`);
+    const detail = bd.data
+    dispatch({ type: GET_PRODUCT_BY_ID, payload: detail });
   };
+};
 
 //---------------------------------------------------------------------//
 //Categories filters actions---------------------------------//
 
-  export const getCategories = () => {
-    return async function (dispatch) {
-      const bd = await axios.get("http://localhost:3001/categories");
-      const categories = bd.data
-      dispatch({ type: GET_CATEGORIES, payload: categories });
-    };
+export const getCategories = () => {
+  return async function (dispatch) {
+    const bd = await axios.get("http://localhost:3001/categories");
+    const categories = bd.data
+    dispatch({ type: GET_CATEGORIES, payload: categories });
   };
+};
 
-  export const filterProductsByCategory = (category) => {
-    return function (dispatch) {
-      dispatch({ type: FILTER_PRODUCTS_BY_CATEGORY, payload: { category } });
-    };
+export const filterProductsByCategory = (category) => {
+  return function (dispatch) {
+    dispatch({ type: FILTER_PRODUCTS_BY_CATEGORY, payload: { category } });
   };
+};
 
 //---------------------------------------------------------------------//
 //Price Filters actions---------------------------------//
-  export const filterProductsByPrice = (minPrice, maxPrice) => {
-    return function (dispatch, getState) {
-      const { products } = getState();
-      const priceFilteredProducts = products.filter(
-        (product) =>
-          parseFloat(product.price) >= parseFloat(minPrice) &&
-          parseFloat(product.price) <= parseFloat(maxPrice)
-      );
-      dispatch({
-        type: FILTER_PRODUCTS_BY_PRICE,
-        payload: { minPrice, maxPrice, filteredProducts: priceFilteredProducts },
-      });
-    };
+export const filterProductsByPrice = (minPrice, maxPrice) => {
+  return function (dispatch, getState) {
+    const { products } = getState();
+    const priceFilteredProducts = products.filter(
+      (product) =>
+        parseFloat(product.price) >= parseFloat(minPrice) &&
+        parseFloat(product.price) <= parseFloat(maxPrice)
+    );
+    dispatch({
+      type: FILTER_PRODUCTS_BY_PRICE,
+      payload: { minPrice, maxPrice, filteredProducts: priceFilteredProducts },
+    });
   };
+};
 
 
 //---------------------------------------------------------------------//
 //Ordenamientos actions---------------------------------//
 
-  export const sortProducts = (order) => {
-    return function (dispatch, getState) {
-      const { filteredProducts } = getState();
-      let sortedProducts = [...filteredProducts];
-      if (order === 'ascending') {
-        sortedProducts.sort((a, b) => a.price - b.price);
-      } else if (order === 'descending') {
-        sortedProducts.sort((a, b) => b.price - a.price);
-      }
-      dispatch({ type: SORT_PRODUCTS, payload: sortedProducts });
-    };
-  };
-  export const actionByName = ()=> {
-    return{
-      type: ACTION_BYNAME
+export const sortProducts = (order) => {
+  return function (dispatch, getState) {
+    const { filteredProducts } = getState();
+    let sortedProducts = [...filteredProducts];
+    if (order === 'ascending') {
+      sortedProducts.sort((a, b) => a.price - b.price);
+    } else if (order === 'descending') {
+      sortedProducts.sort((a, b) => b.price - a.price);
     }
+    dispatch({ type: SORT_PRODUCTS, payload: sortedProducts });
+  };
+};
+export const actionByName = () => {
+  return {
+    type: ACTION_BYNAME
   }
+}
 
 //---------------------------------------------------------------------//
 //Comments actions---------------------------------//
 
-  export const getCommentsByProduc = (id) =>{
+export const getCommentsByProduc = (id) => {
   return async function (dispatch) {
     const response = await axios.get(`http://localhost:3001/comments/product/${id}`);
     const comments = response.data;
@@ -214,34 +285,34 @@ export const getProductById = (id) => {
 //---------------------------------------------------------------------//
 //Ratings actions---------------------------------//
 
-  export const getRatings = () => {
-    return async (dispatch) => {
-      try {
-        const response = await axios.get('http://localhost:3001/ratings/products');
-        const data = response.data.products;
-        dispatch({ type: 'GET_RATINGS', payload: data });
-      } catch (error) {
-        dispatch({ type: 'GET_RATINGS_ERROR', payload: error });
-      }
-    };
+export const getRatings = () => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get('http://localhost:3001/ratings/products');
+      const data = response.data.products;
+      dispatch({ type: 'GET_RATINGS', payload: data });
+    } catch (error) {
+      dispatch({ type: 'GET_RATINGS_ERROR', payload: error });
+    }
   };
+};
 
 
-  export const addRating = (ratingData) => {
-    return async (dispatch) => {
-      try {
-        const response = await axios.post('http://localhost:3001/ratings/add', ratingData);
-        const newRating = response.data;
-        
-        dispatch({
-          type: ADD_RATING,
-          payload: newRating,
-        });
-      } catch (error) {
-        console.log('Error al agregar el rating:', error);
-      }
-    };
+export const addRating = (ratingData) => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.post('http://localhost:3001/ratings/add', ratingData);
+      const newRating = response.data;
+
+      dispatch({
+        type: ADD_RATING,
+        payload: newRating,
+      });
+    } catch (error) {
+      console.log('Error al agregar el rating:', error);
+    }
   };
+};
 
 //---------------------------------------------------------------------//
 //Favorites actions---------------------------------//
@@ -324,27 +395,27 @@ export const getFavoritesByUser = (userId) => {
 
 //---------------------------------------------------------------------//
 //Other actions---------------------------------//
-  export const page = (page) =>{
-    console.log(page)
-    return function  (dispatch){
-      return dispatch({type: PAGE,payload:page})
-    }
-    
+export const page = (page) => {
+  console.log(page)
+  return function (dispatch) {
+    return dispatch({ type: PAGE, payload: page })
   }
- 
-  export function clean(){
-    
-    return{
-     type: CLEAN
-    }
-  
-  };
-  
+
+}
+
+export function clean() {
+
+  return {
+    type: CLEAN
+  }
+
+};
 
 
 
-      
-    
-  
- 
-  
+
+
+
+
+
+
