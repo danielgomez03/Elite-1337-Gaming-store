f711777ccc0dfcc760bbe01fa29354c8e8665d28
NCenVictoria <122835761+NCenVictoria@users.noreply.github.com>
2023-07-07 20:12:57 -0600

    styles

--- client/src/pages/admin/UserAccounts.jsx
+++ client/src/pages/admin/UserAccounts.jsx
@@ -8,7 +8,6 @@ function UserAccounts() {
  dispatch(fetchUsers())
 
   },[])
-  const [buttonStates, setButtonStates] = useState({});
 
   const handleClick = (userId,isActive) => {
     
@@ -20,19 +19,79 @@ function UserAccounts() {
     
   };
   return (
-    <div>UserAccounts
+    
+    <div className='user-list'>UserAccounts
+<style>
+  {`
+      .user-list {
+        width: 100%;
+        max-width: 600px; /* Ajusta el ancho máximo del contenedor principal */
+        margin: 0 auto;
+      }
+    .user-item {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      margin-bottom: 1cm;
+    }
+
+    .user-avatar {
+      width: 50px;
+      height: 50px;
+      object-fit: cover;
+      border-radius: 50%;
+      margin-right: 1cm;
+    }
+
+    .user-info {
+      display: flex;
+      flex-grow: 1;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    .user-name {
+      font-weight: bold;
+    }
+
+    .user-status {
+      padding: 0.2cm;
+      border: none;
+      border-radius: 4px;
+      color: #fff;
+      cursor: pointer;
+      transition: background-color 0.3s ease;
+      flex: 0 0 100px; /* Establece un ancho fijo para los botones */
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .user-status.active {
+      background-color: green;
+    }
+
+    .user-status.inactive {
+      background-color: red;
+    }
+  `}
+</style>
         <div>
-      {users.map((user) => (
-        <div key={user.userId} className="">
-          <span><img src={user.image.url} alt="User Avatar" className="user-avatar" /></span>
-          <span className="user-name">{`${user.firstName} ${user.lastName}`}</span>
-          <button name={user.userId} onClick={() => handleClick(user.userId,user.isActive)}>
-              
-              {user.isActive? 'Activo' : 'Inactivo'}
-            </button>
- 
-        </div>
-      ))}
+        {users.map((user) => (
+  <div key={user.userId} className="user-item">
+    <img src={user.image.url} alt="User Avatar" className="user-avatar" />
+    <div className="user-info">
+      <span className="user-name">{`${user.firstName} ${user.lastName}`}</span>
+      <span>{`${user.firstName} ${user.lastName}`}</span>
+      <button
+        className={`user-status ${user.isActive ? 'active' : 'inactive'}`}
+        onClick={() => handleClick(user.userId, user.isActive)}
+      >
+        {user.isActive ? 'Activo' : 'Inactivo'}
+      </button>
+    </div>
+  </div>
+))}
     </div>
     </div>
   )
