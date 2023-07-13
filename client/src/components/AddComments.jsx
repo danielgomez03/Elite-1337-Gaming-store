365819ba0331f32871e6ce3ad8da32ea86bce05b
NCenVictoria <122835761+NCenVictoria@users.noreply.github.com>
2023-07-12 19:51:55 -0600

    2

--- client/src/components/AddComments.jsx
+++ client/src/components/AddComments.jsx
@@ -8,28 +8,28 @@ export default function AddComments({id}) {
     const userId = useSelector(state=> state.userId)
     // const userId ="ac5b18b6-6383-4a9f-8e4c-65ad3c93b81a"
     const onChange = (e) => {
-
+       
        setContent(e.target.value)
     }
-
+   
     const onSubmitComment=(e) => {
       e.preventDefault();
       const comment = {userId:userId,productId:id,content:content}
       axios.post("http://localhost:3001/comments/add", comment )
       .then(()=>{
-
+        
       setContent("")
       })
       .catch(error=>alert(error.data))
-
-
+      
+  
     }
 
   return (
     <div>
         <form onSubmit={onSubmitComment}>
         <div className="comment-form" style={{ maxWidth: '500px', margin: '0 auto' }}>
-
+         
           <textarea
             onChange={onChange}
             value={content}
