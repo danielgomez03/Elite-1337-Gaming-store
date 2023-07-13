4db65aa19afdfc1dcb8d0f4629847e5e67683c74
NCenVictoria <122835761+NCenVictoria@users.noreply.github.com>
2023-07-13 12:24:42 -0600

    button productsadd

--- client/src/pages/admin/ProductsAdmin.jsx
+++ client/src/pages/admin/ProductsAdmin.jsx
@@ -54,7 +54,7 @@ function ProductsAdmin() {
         <button className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
         onClick={openCreateProduct}>Add Product</button>
       </div>
-      { setSelectedButton && <CreateProduct closeCreateProduct={closeCreateProduct} /> }
+      { selectedButton && <CreateProduct closeCreateProduct={closeCreateProduct} /> }
       <div className="product-list">
         <table className="w-full border-collapse relative">
           <thead className='sticky'>
